23d248d72403b9a45c1f9b844c595d2b7d1960e0
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-14 17:31:24 +0300

    Update models.py

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -94,7 +94,7 @@ class IngredientAmount(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='amount_recipe',
+        related_name='ingredients_amount',
     )
     ingredient = models.ForeignKey(
         Ingredient,
