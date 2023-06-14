4d8e9efae6a446e5ad596fd1a3e8db88ecaef37c
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-15 01:55:25 +0300

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -77,7 +77,7 @@ class SubscriptionSerializer(serializers.ModelSerializer):
         return Follow.objects.filter(user=user, author=obj.id).exists()
 
     def get_recipes_count(self, obj):
-        return obj.recipe.count()
+        return obj.recipes.count()
 
 
 class SubscriptionCreateSerializer(serializers.ModelSerializer):
