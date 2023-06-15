b87caeab37e35215c9e3195866cccfd0d6b6c5a8
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-15 16:37:03 +0300

    Update admin.py

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 from recipes.models import (Recipe, Tag, Ingredient,
-                            IngredientAmount, Favourite, ShoppingCart)
+                            RecipeIngredient, Favourite, ShoppingCart)
 
 
-class IngredientAmountInstanceInline(admin.TabularInline):
+class RecipeIngredientInstanceInline(admin.TabularInline):
     model = IngredientAmount
     extra = 1
     min_num = 1
