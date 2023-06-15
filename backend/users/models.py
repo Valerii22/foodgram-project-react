dce9a76a4ac9c86b53684d09597ee89ac58db59e
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-15 17:34:42 +0300

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -44,29 +44,32 @@ class User(AbstractUser):
         return self.username
 
 
-class Subscribe(models.Model):
+class Follow(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='subscriber',
-        verbose_name='Подписчик'
-    )
+        related_name='follower',
+        verbose_name='Подписчик')
+
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='subscribing',
-        verbose_name='Подписан'
-    )
-
-    def __str__(self):
-        return f'{self.user.username} - {self.author.username}'
+        related_name='following',
+        verbose_name='Автор')
 
     class Meta:
-        verbose_name = 'Подписка на авторов'
-        verbose_name_plural = 'Подписки на авторов'
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
         constraints = [
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('author')),
+                name='no_self_subscribe'
+            ),
             models.UniqueConstraint(
                 fields=['user', 'author'],
-                name='unique_subscribe'
+                name='unique_following'
             )
         ]
+
+    def __str__(self):
+        return f'Подписка {self.user} на {self.author}'
