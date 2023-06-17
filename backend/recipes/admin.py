28c50ff4b98a094745197c3120ba32f45e7de31b
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-17 20:11:57 +0300

    Update admin.py

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,44 +1,45 @@
 from django.contrib import admin
 
-from recipes.models import (Recipe, Tag, Ingredient,
-                            IngredientAmount, Favourite, ShoppingCart)
-
-
-class IngredientAmountInstanceInline(admin.TabularInline):
-    model = IngredientAmount
-    extra = 1
-    min_num = 1
-
-
-@admin.register(Recipe)
-class RecipesAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'author',
-    )
-    inlines = [IngredientAmountInstanceInline]
-    list_filter = ('name', )
-
-
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'slug',)
-    list_filter = ('name', 'slug',)
-
-
-@admin.register(Ingredient)
-class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', )
-    list_filter = ('name', )
+from recipes.models import (Favourite, Ingredient, Recipe, RecipeIngredient,
+                            ShoppingCart, Tag)
 
 
 @admin.register(Favourite)
 class FavouriteAdmin(admin.ModelAdmin):
     list_display = ('user', 'recipe',)
-    list_filter = ('user', )
+    search_fields = ('user', 'recipe',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'measurement_unit')
+    search_fields = ('name', 'measurement_unit')
+    list_filter = ('name', 'measurement_unit')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'author', 'text',
+                    'cooking_time', 'image', 'date')
+    search_fields = ('name', 'author', 'text', 'cooking_time')
+    list_filter = ('name', 'author', 'tags')
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'recipe', 'ingredient', 'amount')
+    search_fields = ('recipe', 'ingredient')
+    list_filter = ('recipe', 'ingredient')
 
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = ('user', 'recipe',)
-    list_filter = ('user', )
+    search_fields = ('user', 'recipe',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'color', 'slug')
+    search_fields = ('name', 'color', 'slug')
+    list_filter = ('name', 'color', 'slug')
