4d299a0dfde29f7935e563a0040d27825a13e95d
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-18 00:20:16 +0300

    Update utils.py

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,20 +1,26 @@
-from django.db.models.aggregates import Sum
+from django.db.models import Sum
 from django.http import HttpResponse
 
-from .serializers import RecipeIngredient
+from recipes.models import RecipeIngredient
 
 
-def download_shopping_cart(request):
-    ingredients = RecipeIngredient.objects.filter(
-        recipe__shopping_cart__user=request.user).values(
-            'ingredient__name', 'ingredient__measurement_unit').annotate(
-                amount=Sum('total_amount'))
+def download_shopping_cart(self, request):
+    ingredients = (
+        RecipeIngredient.objects
+        .filter(recipe__shopping__user=request.user)
+        .values('ingredient__name', 'ingredient__measurement_unit')
+        .annotate(amount=Sum('amount'))
+    )
     text = ''
     for ingredient in ingredients:
-        text += (f'•  {ingredient["ingredient__name"]}'
-                 f'({ingredient["ingredient__measurement_unit"]})'
-                 f'— {ingredient["total_amount"]}\n')
+        text += (
+            f'•  {ingredient["ingredient__name"]}'
+            f'({ingredient["ingredient__measurement_unit"]})'
+            f'— {ingredient["amount"]}\n'
+        )
     headers = {
-        'Content-Disposition': 'attachment; filename=cart.txt'}
+        'Content-Disposition': 'attachment; filename=shopping_cart.txt'
+    }
     return HttpResponse(
-        text, content_type='text/plain; charset=UTF-8', headers=headers)
+        text, content_type='text/plain; charset=UTF-8', headers=headers
+    )
