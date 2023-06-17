e5cf32a2b5e868f603e2e9d6553781e811b11f55
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-18 02:57:20 +0300

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -66,15 +66,15 @@ class SubscribeSerializer(serializers.ModelSerializer):
             author=obj.id, user=user).exists()
 
     def get_recipes(self, obj):
-        request = self.context.get('request')
-        recipes_limit = None
-        if request:
-            recipes_limit = request.query_params.get('recipes_limit')
-        recipes = obj.recipes.all()
-        if recipes_limit:
-            recipes = obj.recipes.all()[:int(recipes_limit)]
-        serializer = ShortRecipeSerializer(recipes, many=True)
-        return serializer.data
+        """Получение списка рецептов автора"""
+        limit = self.context['request'].query_params.get(
+            'recipes_limit', settings.COUNT_RECIPES
+        )
+        recipe_obj = obj.author.recipes.all()
+        recipe_obj = obj.recipes.all()
+        if limit:
+            recipe_obj = recipe_obj[:int(limit)]
+        serializer = ShortRecipeSerializer(recipe_obj, many=True)
 
 
 class ShortRecipeSerializer(ModelSerializer):
