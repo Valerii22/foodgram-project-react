58978e3a27465bb5a13ec676562dd17103bebcae
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-17 20:09:37 +0300

    Update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,29 +1,34 @@
-from django_filters.rest_framework import FilterSet, filters
+from django.contrib.auth import get_user_model
+from django_filters import rest_framework
+from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe, Tag, Ingredient
+from recipes.models import Recipe
 
 
-class RecipeFilter(FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all(),
-    )
-    is_favorited = filters.NumberFilter(method='filter_is_favorited')
+User = get_user_model()
+
+
+class NameSearchFilter(SearchFilter):
+    search_param = 'name'
+
+
+class RecipeFilter(rest_framework.FilterSet):
+    author = rest_framework.ModelChoiceFilter(queryset=User.objects.all())
+    tags = rest_framework.AllValuesMultipleFilter(field_name='tags__slug')
+    is_favorited = rest_framework.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = rest_framework.BooleanFilter(
+        method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited')
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(favourites__user=self.request.user)
         return queryset
 
-
-class IngredientFilter(FilterSet):
-    name = filters.CharFilter(lookup_expr='startswith')
-
-    class Meta:
-        model = Ingredient
-        fields = ('name',)
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
