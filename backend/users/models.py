b37e35e734482cc34c05734dd92c62b1abd7422d
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-17 20:13:39 +0300

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,75 +1,62 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
-from django.conf import settings
+from django.db.models import UniqueConstraint
 
 
 class User(AbstractUser):
+    '''Модель пользователя'''
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [
-        'username',
-        'first_name',
-        'last_name',
-    ]
-    username = models.CharField(
-        verbose_name='Логин',
-        max_length=150,
-        unique=True,
-        error_messages={
-            'unique': 'Пользователь с таким никнеймом уже существует!',
-        },
-        help_text='Укажите свой никнейм'
-    )
-    first_name = models.CharField(
-        blank=False,
-        max_length=150,
-        verbose_name='First Name',
-    )
-    last_name = models.CharField(
-        blank=False,
-        max_length=150,
-        verbose_name='Last Name',
-    )
-    email = models.EmailField(
-        'email address',
-        max_length=settings.MAX_EMAIL_LENGTH,
-        unique=True,
-    )
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    email = models.EmailField(verbose_name='Почта',
+                              max_length=254,
+                              unique=True)
+    username = models.CharField(verbose_name='Имя пользователя',
+                                unique=True,
+                                validators=[UnicodeUsernameValidator],
+                                max_length=150)
+    first_name = models.CharField(verbose_name='Имя',
+                                  max_length=150)
+    last_name = models.CharField(verbose_name='Фамилия',
+                                 max_length=150)
+    password = models.CharField(verbose_name='Пароль',
+                                max_length=150)
 
     class Meta:
-        ordering = ['id']
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        ordering = ('pk',)
+        constraints = (
+            models.UniqueConstraint(fields=('email', 'username'),
+                                    name='unique_auth'),
+        )
 
     def __str__(self):
         return self.username
 
 
 class Follow(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='follower',
-        verbose_name='Подписчик')
+    '''Модель подписок'''
 
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='following',
-        verbose_name='Автор')
+    user = models.ForeignKey(User,
+                             verbose_name='Подписчик',
+                             on_delete=models.CASCADE,
+                             related_name='follower')
+    author = models.ForeignKey(User,
+                               verbose_name='Автор',
+                               on_delete=models.CASCADE,
+                               related_name='following')
 
     class Meta:
-        verbose_name = 'Подписка'
-        verbose_name_plural = 'Подписки'
-        constraints = [
-            models.CheckConstraint(
-                check=~models.Q(user=models.F('author')),
-                name='no_self_subscribe'
-            ),
-            models.UniqueConstraint(
-                fields=['user', 'author'],
-                name='unique_following'
-            )
-        ]
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
+        ordering = ('-pk',)
+        constraints = (
+            UniqueConstraint(fields=('user', 'author'),
+                             name='unique_subscription'),
+        )
 
     def __str__(self):
-        return f'Подписка {self.user} на {self.author}'
+        return self.user
