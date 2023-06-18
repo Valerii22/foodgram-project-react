8a0dee78708b1315aa2ad5d03b7d89b3e6569d54
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-18 14:15:00 +0300

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -27,8 +27,7 @@ class CurrentUserViewSet(UserViewSet):
 
     @action(detail=False,
             methods=['GET'],
-            permission_classes=[IsAuthenticated],
-            pagination_class=CustomPagination)
+            permission_classes=[IsAuthenticated])
     def subscriptions(self, request):
         user = request.user
         queryset = User.objects.filter(following__user=user)
