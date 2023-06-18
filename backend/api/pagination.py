05fc3b673784fa2fd74543b3866fb9cd0a07d3cc
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-18 15:02:19 +0300

    Update pagination.py

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,4 +2,10 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
+    page_size = 9
+    page_size_query_param = 'limit'
+    
+    
+class CustomSubscribePagination(PageNumberPagination):
+    page_size = 3
     page_size_query_param = 'limit'
