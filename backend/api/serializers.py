28d3f1cd04d6e2cb4b49c4db68c1b857c0d739a0
Valerii22 <112119532+Valerii22@users.noreply.github.com>
2023-06-18 12:49:21 +0300

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -43,39 +43,21 @@ class CurrentUserSerializer(serializers.ModelSerializer):
         return user
 
 
-class SubscribeSerializer(serializers.ModelSerializer):
-    email = serializers.ReadOnlyField(source='author.email')
-    id = serializers.ReadOnlyField(source='author.id')
-    username = serializers.ReadOnlyField(source='author.username')
-    first_name = serializers.ReadOnlyField(source='author.first_name')
-    last_name = serializers.ReadOnlyField(source='author.last_name')
-    recipes = serializers.SerializerMethodField()
-    recipes_count = serializers.ReadOnlyField(source='author.recipes.count')
-    is_subscribed = serializers.SerializerMethodField()
+class SubscribeSerializer(MyUserSerializer):
+    '''Сериализатор подписoк'''
+
+    recipes_count = serializers.IntegerField(source='recipes.count',
+                                             read_only=True)
+    recipes = ShortRecipeSerializer(many=True, read_only=True)
+    is_subscribed = serializers.BooleanField(default=True)
 
     class Meta:
-        model = Follow
-        fields = (
-            'email', 'id', 'username', 'first_name', 'last_name',
-            'is_subscribed', 'recipes', 'recipes_count'
-        )
-
-    def get_is_subscribed(self, obj):
-        """Статус подписки на автора"""
-        user = self.context.get('request').user
-        return Follow.objects.filter(
-            author=obj.id, user=user).exists()
-
-    def get_recipes(self, obj):
-        """Получение списка рецептов автора"""
-        limit = self.context['request'].query_params.get(
-            'recipes_limit', settings.COUNT_RECIPES
-        )
-        recipe_obj = obj.recipes.all()
-        if limit:
-            recipe_obj = recipe_obj[:int(limit)]
-        serializer = ShortRecipeSerializer(recipe_obj, many=True)
-        return serializer.data
+        model = User
+        fields = ('id', 'email', 'username', 'first_name',
+                  'last_name', 'recipes_count', 'recipes',
+                  'is_subscribed')
+        read_only_fields = ('email', 'username',
+                            'first_name', 'last_name')
 
 
 class ShortRecipeSerializer(ModelSerializer):
